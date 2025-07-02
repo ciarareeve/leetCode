d12bd8e13cbbd2632157f62a47c583445bc798e3
ciarareeve <77694019+ciarareeve@users.noreply.github.com>
2025-07-01 17:16:43 -0700

    Time: 26 ms (30.10%) | Memory: 13.6 MB (32.28%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -5,12 +5,6 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        ana_dict = {}
-        if s not in ana_dict:
-            ana_dict[''.join(sorted(s))] = s
-        if ''.join(sorted(t)) in ana_dict:
-            return True
-        else:
-            return False
+        return sorted(s) == sorted(t)
 
         
